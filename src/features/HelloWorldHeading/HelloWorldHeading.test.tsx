0177d20576e6d0d7be453c9adb5fdfaa89219823
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2020-02-01 15:08:20 +0100

    Added class version

--- src/features/HelloWorldHeading/HelloWorldHeading.test.tsx
+++ src/features/HelloWorldHeading/HelloWorldHeading.test.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { render, fireEvent } from "@testing-library/react"
-import { HelloWorldHeading } from "./HelloWorldHeading"
+import HelloWorldHeading from "./"
 
 describe("HelloWorldHeading", () => {
 	it("is defined", () => {
@@ -14,7 +14,14 @@ describe("HelloWorldHeading", () => {
 	it("shows Hello World when clicked", () => {
 		const { getByText } = render(<HelloWorldHeading />)
 		fireEvent.click(getByText(/Click me/i))
-		const helloWorldElement = getByText(/Hello World/)
-		expect(helloWorldElement).toBeInTheDocument()
+		expect(getByText(/Hello World/)).toBeInTheDocument()
+	})
+	it("hides Hello World when clicked twice", () => {
+		const { queryByText, getByText } = render(<HelloWorldHeading />)
+		fireEvent.click(getByText(/Click me/i))
+		expect(getByText(/Hello World/)).toBeInTheDocument()
+
+		fireEvent.click(getByText(/Click me/i))
+		expect(queryByText(/Hello World/i)).not.toBeInTheDocument()
 	})
 })
