72b74f4a90f2a0735648beac27a045646aa7f61b
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2020-10-04 17:34:56 +0200

    Removed classnames obsolete ref

--- src/features/Nav/Nav.tsx
+++ src/features/Nav/Nav.tsx
@@ -2,29 +2,29 @@ import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from "@fortawesome/free-solid-svg-icons"
-import cn from "classnames"
 
 import cls from "./Nav.module.css"
 
 export const Nav = (): JSX.Element => {
 	const [visible, setVisible] = useState(false)
-
 	const close = () => setVisible(false)
 
 	return (
-		<div className={cls.container}>
+		<>
+			<div className={`${cls.container} ${visible && cls.containerVisible}`}>
+				<nav className={`${cls.nav} ${visible && cls.navVisible}`}>
+					<div>
+						<Link to="/" onClick={close}>Home</Link>
+					</div>
+					<div>
+						<Link to="/propstate" onClick={close}>PropState</Link>
+					</div>
+				</nav>
+			</div>
 			<button className={cls.btn} onClick={() => setVisible(!visible)}>
 				<FontAwesomeIcon icon={faBars} size="2x"/>
 			</button>
-			<nav className={cn(cls.nav, {[cls.navVisible]: visible})}>
-				<div>
-					<Link to="/" onClick={close}>Home</Link>
-				</div>
-				<div>
-					<Link to="/propstate" onClick={close}>PropState</Link>
-				</div>
-			</nav>
-		</div>
+		</>
 	)
 }
 
