41286a32757c070740664f542af65548f748baeb
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2020-10-08 08:18:14 +0200

    Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from "react"
 import Router from "features/Router"
 import Nav from "features/Nav"
 import { BrowserRouter } from "react-router-dom"
+import Layout from "features/Layout"
 
 function App() {
   return (
     <BrowserRouter>
       <Nav/>
-      <Router/>
+      <Layout footer={<p>Ciber React Course</p>}>
+        <Router/>
+      </Layout>
     </BrowserRouter>
   )
 }
