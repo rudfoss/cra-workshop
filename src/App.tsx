f8bd7f51fa15df3ddc4089e2227e71558122c422
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2020-12-02 17:57:58 +0100

    Added recent template

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from "react"
-import HelloWorld from "features/HelloWorld"
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App: React.FC = () => {
-	return (
-		<div>
-			<h1>React workshop</h1>
-			<HelloWorld />
-		</div>
-	)
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
