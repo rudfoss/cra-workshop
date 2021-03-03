2b372144d4dd10876440ad7f8a1de3bbf01b6a84
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2021-03-03 13:11:03 +0100

    Messages

---
+++ src/messages.ts
@@ -0,0 +1,15 @@
+export const messages = [
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"Mauris tristique libero eu ex sagittis, ac bibendum ipsum pellentesque.",
+	"Nunc et felis ac libero eleifend varius."
+]
+
+export const messageObj = {
+	"consectetur": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	"sagittis": "Mauris tristique libero eu ex sagittis, ac bibendum ipsum pellentesque.",
+	"eleifend": "Nunc et felis ac libero eleifend varius.",
+	"volutpat": "Donec molestie leo nec nibh volutpat, eget consequat nunc elementum.",
+	"lorem": "Phasellus euismod lorem vel leo semper, sed vehicula augue vehicula.",
+	"ipsum": "Nulla porta dolor a leo commodo, et ornare orci auctor.",
+	"dolor": "Aenean efficitur leo ac lorem imperdiet tincidunt."
+}
