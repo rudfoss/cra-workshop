09227affda1ebda675931d764b2b345d67f45066
Thomas Haugland Rudfoss <thomas.rudfoss@gmail.com>
2021-03-05 15:30:25 +0100

    CodeSplitting

--- src/routes/BaseRoute.tsx
+++ src/routes/BaseRoute.tsx
@@ -1,67 +1,69 @@
 import { Route, Switch } from "react-router"
-import EitherAOrB from "tasks/EitherAOrB"
-import Incrementor from "tasks/Incrementor"
+import { lazy, Suspense } from "react"
+
 import ListMessages from "tasks/ListMessages"
 import TodoList from "tasks/TodoList"
-import BigBorder from "UI/BigBorder"
 import ArticleExample from "./ArticleExample"
-
 import { MessageViewerRoute } from "tasks/MessageViewer"
-import UseEffectExample from "tasks/UseEffectExample"
 import DelayDisplay from "tasks/DelayDisplay"
 import AsyncExample from "tasks/AsyncExample"
 import { ListPostsRoute } from "tasks/ListPosts"
 
+const Incrementor = lazy(() => import("tasks/Incrementor"))
+const EitherAOrB = lazy(() => import("tasks/EitherAOrB"))
+const BigBorder = lazy(() => import("UI/BigBorder"))
+
 export const BaseRoute = () => {
 	return (
-		<Switch>
-			<Route path="/article/:articleId">
-				<ArticleExample/>
-			</Route>
+		<Suspense fallback={<p>Loading...</p>}>
+			<Switch>
+				<Route path="/article/:articleId">
+					<ArticleExample/>
+				</Route>
 
-			<Route path="/message/:messageId">
-				<MessageViewerRoute />
-			</Route>
-			<Route path="/message">
-				<ListMessages/>
-			</Route>
+				<Route path="/message/:messageId">
+					<MessageViewerRoute />
+				</Route>
+				<Route path="/message">
+					<ListMessages/>
+				</Route>
 
-			<Route path="/incrementor">
-				<UseEffectExample/>
-				<Incrementor/>
-			</Route>
-			<Route path="/bigBorder">
-				<BigBorder borderSize={10}>
-					<p>Bordered</p>
-				</BigBorder>
-			</Route>
-			<Route path="/eitherAOrB">
-				<EitherAOrB a="foo" b="bar"/>
-			</Route>
-			<Route path="/todoList">
-				<TodoList/>
-			</Route>
-			<Route path="/delayDisplay">
-				<DelayDisplay message="Hello world"/>
-			</Route>
-			<Route path="/asyncExample">
-				<AsyncExample/>
-			</Route>
-			
-			<Route path="/posts/:sortOrder">
-				<ListPostsRoute />
-			</Route>
-			<Route path="/posts">
-				<ListPostsRoute />
-			</Route>
+				<Route path="/incrementor">
+					<Incrementor/>
+				</Route>
+				<Route path="/bigBorder">
+					<BigBorder borderSize={10}>
+						<p>Bordered</p>
+					</BigBorder>
+				</Route>
+				<Route path="/eitherAOrB">
+					<EitherAOrB a="foo" b="bar"/>
+				</Route>
+				<Route path="/todoList">
+					<TodoList/>
+				</Route>
+				<Route path="/delayDisplay">
+					<DelayDisplay message="Hello world"/>
+				</Route>
+				<Route path="/asyncExample">
+					<AsyncExample/>
+				</Route>
+				
+				<Route path="/posts/:sortOrder">
+					<ListPostsRoute />
+				</Route>
+				<Route path="/posts">
+					<ListPostsRoute />
+				</Route>
 
-			<Route path="/" exact>
-				<p>Home</p>
-			</Route>
-			<Route>
-				<p>Are you lost?</p>
-			</Route>
-		</Switch>
+				<Route path="/" exact>
+					<p>Home</p>
+				</Route>
+				<Route>
+					<p>Are you lost?</p>
+				</Route>
+			</Switch>
+		</Suspense>
 	)
 }
 
