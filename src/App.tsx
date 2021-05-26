c33fb563947e73cba4d1a561ed30611d73896d32
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2021-05-26 15:10:16 +0200

    ListUsers

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import { LoopExample } from './LoopExample';
+import { ListUsers } from './ListUsers';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <LoopExample/>
+        <ListUsers />
       </header>
     </div>
   );
