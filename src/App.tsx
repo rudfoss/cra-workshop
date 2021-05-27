bce2b871b0d4e2b55ce28c4bc69e2cedc7ba1607
Thomas Haugland Rudfoss <thomas.rudfoss@bouvet.no>
2021-05-27 14:41:23 +0200

    Added footer composition prop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import TodoList from 'tasks/TodoList';
+import BigBorder from 'tasks/BigBorder';
 import './App.css';
 // import Task from 'tasks/Repeat';
 
@@ -7,7 +7,9 @@ function App() {
   return (
     <div className="App" >
       <header className="App-header">
-        <TodoList />
+        <BigBorder footer={<p>I am the footer</p>}>
+          <h2>This is inside a border</h2>
+        </BigBorder>
       </header>
     </div>
   );
--- src/tasks/BigBorder/BigBorder.tsx
+++ src/tasks/BigBorder/BigBorder.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 
 interface BigBorderProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+	footer?: React.ReactNode
+	
 	/**
 	 * The size of the border in pixels.
 	 */
@@ -22,16 +24,25 @@ interface BigBorderProps extends React.DetailedHTMLProps<React.HTMLAttributes<HT
  * ```
  */
 export const BigBorder = (props: BigBorderProps) => {
-	const { size = 5, children, ...rest} = props
+	const { footer, size = 5, children, ...rest} = props
 
-	return React.createElement("div", {
-		style: {
-			border: `${size}px solid hotpink`,
-			borderRadius: "5px"
-		},
-		...rest
-	}, children)
-	// <div style={{border: `${size}px solid hotpink`, borderRadius: "5px"}} {...rest}>
-	// 	{children}
-	// </div>
+	// return React.createElement("div", {
+	// 	style: {
+	// 		border: `${size}px solid hotpink`,
+	// 		borderRadius: "5px"
+	// 	},
+	// 	...rest
+	// }, children)
+	return (
+		<div style={{border: `${size}px solid hotpink`, borderRadius: "5px"}} {...rest}>
+			<section>
+				{children}
+			</section>
+			{footer && (
+				<footer>
+					{footer}
+				</footer>
+			)}
+		</div>
+	)
 }
